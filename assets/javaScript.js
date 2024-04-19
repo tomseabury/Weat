46f21de2cc4bfb3f9208beae989fa3533ec9e4e6
tomseabury <Tommy.seabury@gmail.com>
2024-04-19 16:13:07 -0600

    Firebase

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -1,5 +1,19 @@
 $(()=> {
-   
+
+    var config = {
+        apiKey: "AIzaSyD2odY5xcmS2nSXzWYwO16xg90kF-GfF4s",
+        authDomain: "weat-bde5f.firebaseapp.com",
+        databaseURL: "https://weat-bde5f-default-rtdb.firebaseio.com",
+        projectId: "weat-bde5f",
+        storageBucket: "weat-bde5f.appspot.com",
+        messagingSenderId: "760459252475",
+        appId: "1:760459252475:web:fd68f7fc096d73c1ded65e",
+        measurementId: "G-RXLEC4XSB0"
+      };
+    firebase.initializeApp(config);
+
+    
+    
     var database = firebase.database();
     // Button click event to display data
     $("#displayDataBtn").click(() => {
